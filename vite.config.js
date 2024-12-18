8eec88be9a8b2108c7a069dfd332435aecc3d340
Patrick Long <longpatrick3317@gmail.com>
2024-12-18 15:20:56 -0500

    sus

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Make sure to listen on all network interfaces
+    port: 3000,        // Optional: You can specify the port or use the default port
+  },
 })
